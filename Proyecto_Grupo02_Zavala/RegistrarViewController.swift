db574998f18ee7f32ad933f4ca2c944c56c85689
Gonzalo <gonzalol2424@gmail.com>
2025-04-14 17:44:32 -0500

    Segundo Commit

---
+++ Proyecto_Grupo02_Zavala/RegistrarViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RegistrarViewController.swift
+//  Proyecto_Grupo02_Zavala
+//
+//  Created by DAMII on 14/04/25.
+//
+
+import UIKit
+
+class RegistrarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    
+    
+    @IBAction func volverTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+ 
+}
